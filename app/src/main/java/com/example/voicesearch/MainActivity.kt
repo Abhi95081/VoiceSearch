c7523b43345a9f2c6f22f5b50e324a89d6bfb426
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-12-06 14:16:30 +0530

    main commit

--- app/src/main/java/com/example/voicesearch/MainActivity.kt
+++ app/src/main/java/com/example/voicesearch/MainActivity.kt
@@ -4,23 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.voicesearch.Screens.MainScaffold
+import com.example.voicesearch.Screens.SplashScreen
 import com.example.voicesearch.ui.theme.VoiceSearchTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             VoiceSearchTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                // App-level state
+                var showSplash by remember { mutableStateOf(true) }
 
+                if (showSplash) {
+                    SplashScreen {
+                    }
+                } else {
+                    MainScaffold()
                 }
             }
         }
