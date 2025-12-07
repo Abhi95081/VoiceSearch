bb0ac9db62a14b8b40ccfed4ab829f113af9e784
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-12-07 15:00:11 +0530

    main commit

--- app/src/main/java/com/example/voicesearch/MainActivity.kt
+++ app/src/main/java/com/example/voicesearch/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,19 +13,21 @@ import com.example.voicesearch.Screens.SplashScreen
 import com.example.voicesearch.ui.theme.VoiceSearchTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
             VoiceSearchTheme {
-                // App-level state
                 var showSplash by remember { mutableStateOf(true) }
 
                 if (showSplash) {
-                    SplashScreen {
-                    }
+                    SplashScreen(
+                        onFinished = {
+                            showSplash = false
+                        }
+                    )
                 } else {
                     MainScaffold()
                 }
